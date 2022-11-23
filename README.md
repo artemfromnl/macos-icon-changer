ed9bf0ca0109288b73b00ca67fd7e06303b6d235
Artem Smirnov <artemfromnl@gmail.com>
2022-11-23 21:47:31 +0700

    feat: add icons for jetbrains ide

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # macOS icon changer
 
 Script that automatically replaces macOS app icons
-
-***TODO:***
-* Add support for JetBrains IDEs, they are in a different dir
-* Add icons for system apps
--- changer.sh
+++ changer.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Permission denied"
-    exit
-fi
-
 for icon in assets/*.icns; do
   name=$(echo "$icon" | sed -e "s/^assets\///" -e "s/.icns$//")
   fileicon set "/Applications/$name.app" "$icon"
+done
+
+for icon in assets/jetbrains/*.icns; do
+  name=$(echo "$icon" | sed -e "s/^assets\/jetbrains\///" -e "s/.icns$//")
+  fileicon set "$HOME/Applications/JetBrains Toolbox/$name.app" "$icon"
+
+  find "$HOME/Library/Application Support/JetBrains/Toolbox" -name "$name*.app" |
+  while read -r app; do
+    fileicon set "$app" "$icon"
+  done
 done
